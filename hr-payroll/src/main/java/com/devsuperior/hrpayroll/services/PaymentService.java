bab28dff39c32eb76249e8d36fcd291b65269c60
Inrikys <inrikys@gmail.com>
2021-09-13 23:00:46 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -4,24 +4,15 @@ import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
 import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
     WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
